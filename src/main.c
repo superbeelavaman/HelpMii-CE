8863054f981be54ca9b253cede41255e270d1064
Logan Joy-Koer (SuperbeeLavaman) <loganlee.jk@outlook.com>
2025-06-10 10:31:59 -0700

    Write program information and licensing.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 /*
  *--------------------------------------
- * Program Name:
- * Author:
- * License:
- * Description:
+ * Program Name: HelMmii for TI-84 Plus CE
+ * Author: Techflash, Tech64, SuperbeeLavaman
+ * License: GNU General Public License version 2
+ * Description: Port of Wii-Linux HelpMii for TI-84 Plus CE
  *--------------------------------------
 */
 
